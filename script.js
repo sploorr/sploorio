d22ddc730ee06a0614dbe119c64cdc52c6add38b
sploorr <115745797+sploorr@users.noreply.github.com>
2022-12-05 17:04:42 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function changeBG(bgImage) {
+    document.getElementById("bgImage").src = bgImage;
+}
+
+function changeHome(homeImage) {
+    document.getElementById("homeImage").src = homeImage;
+}
+
+/*
+function changeBGAndHome(bgImage, homeImage) {
+  document.getElementById("bgImage").src = bgImage;
+  document.getElementById("homeImage").src = homeImage;
+}
+*/
+
+/*
+window.addEventListener("load", function() {
+  document.getElementById("menu").classList.add("fade-in");
+});
+*/
